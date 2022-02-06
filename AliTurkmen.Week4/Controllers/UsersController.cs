5489406eb999c6b22838da62fc5721ee15ce602e
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-02-06 06:47:17 +0300

    Add files via upload

---
+++ AliTurkmen.Week4/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jwtCoreDemo.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace jwtCoreDemo.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.UserName, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Kullanici veya şifre hatalı!" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register(User user)
+        {
+            bool isUserExist;
+            isUserExist = _userService.IsUserExist(user);
+
+            if (isUserExist)
+                return BadRequest("Kullanıcı adı sistemde kayıtlıdır.");
+
+            var users = _userService.Insert(user);
+            return Ok(users);
+        }
+    }
+}
